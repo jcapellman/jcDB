2cea1157dcd6ea0ab0656c2dc0206d02f64ab731
Jarred Capellman <jcapellman@hotmail.com>
2019-03-06 20:19:15 -0800

    Added: PerformanceTests class

---
+++ jcDB.UnitTests/PerformanceTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using jcDB.lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jcDB.UnitTests
+{
+    [TestClass]
+    public class PerformanceTests
+    {
+        private string PERF_DB_FILENAME = "perf.db";
+
+        private Dictionary<string, object> Initialize(int dataSize)
+        {
+            PERF_DB_FILENAME = $"perf_{dataSize}.db";
+
+            var dict = new Dictionary<string, object>();
+
+            return dict;
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            if (File.Exists(PERF_DB_FILENAME))
+            {
+                File.Delete(PERF_DB_FILENAME);
+            }
+        }
+
+        [TestMethod]
+        public void InsertTen()
+        {
+            var data = Initialize(10);
+
+            var start = DateTime.Now;
+
+            var db = new Database(PERF_DB_FILENAME);
+
+            foreach (var key in data.Keys)
+            {
+                db.InsertFireAndForget(key, data[key]);    
+            }
+
+            Console.WriteLine(DateTime.Now.Subtract(start).TotalSeconds);
+        }
+    }
+}
