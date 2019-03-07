ce68f41c28b06d2c570ac2b05b6a15f452eb0a3e
Jarred Capellman <jcapellman@hotmail.com>
2019-03-06 20:11:39 -0800

    Added: Write Worker Test

--- jcDB.UnitTests/SanityTests.cs
+++ jcDB.UnitTests/SanityTests.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 using jcDB.lib;
@@ -9,6 +11,17 @@ namespace jcDB.UnitTests
     [TestClass]
     public class SanityTests
     {
+        private const string DB_FILENAME = "testing.db";
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            if (File.Exists(DB_FILENAME))
+            {
+                File.Delete(DB_FILENAME);
+            }
+        }
+
         [TestMethod]
         public void SimpleInsertAndRetrieveNonFireAndForget()
         {
@@ -39,6 +52,23 @@ namespace jcDB.UnitTests
             var result = db.Get(key);
 
             Assert.AreEqual(val, result);
+        }
+
+        [TestMethod]
+        public async Task VerifyWriteWorker()
+        {
+            var key = "testkey";
+            var val = "testval";
+
+            var db = new Database(DB_FILENAME, 1);
+
+            var insertResult = await db.InsertAsync(key, val);
+
+            Assert.IsTrue(insertResult);
+
+            Thread.Sleep(1000);
+
+            Assert.IsTrue(File.Exists(DB_FILENAME));
         }
     }
 }
