d1a009b09b6f32b31d1b8504e235a36f57c31cd4
Jarred Capellman <jcapellman@hotmail.com>
2019-03-08 23:15:53 -0600

    Added: Benchmark App

---
+++ jcDB.Benchmark.app/MainBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+using BenchmarkDotNet.Attributes;
+
+using jcDB.lib;
+
+namespace jcDB.Benchmark.app
+{
+    [CoreJob(true)]
+    [RPlotExporter, RankColumn]
+    public class MainBenchmark
+    {
+        private Database db = new Database();
+
+        private Dictionary<string, object> _data;
+
+        [Params(100, 1000, 10000, 100000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _data = new Dictionary<string, object>();
+
+            for (var x = 0; x < N; x++)
+            {
+                _data.Add(x.ToString(), x);
+            }
+        }
+
+        [Benchmark]
+        public void InsertMany()
+        {
+            foreach (var (key, value) in _data)
+            {
+                db.InsertFireAndForget(key, value);
+            }
+        }
+    }
+}
---
+++ jcDB.Benchmark.app/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace jcDB.Benchmark.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MainBenchmark>();
+        }
+    }
+}
